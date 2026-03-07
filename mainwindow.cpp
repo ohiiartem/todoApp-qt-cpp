bf1a271cbce2a62db895105292f48221071a48df
Artem <ogijartem7@gmail.com>
2026-03-07 13:22:39 +0100

    feat: add AppState machine with Empty and CreatingTask states

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,39 @@
 #include "mainwindow.h"
-#include <QVBoxLayout>
-
+#include <QKeyEvent>
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+    : QMainWindow(parent),
+    currentState(AppState::Empty)
 {
     setFixedSize(1400, 750);
+    setState(AppState::Empty);
 }
-
 MainWindow::~MainWindow() {}
+void MainWindow::setState(AppState newState)
+{
+    currentState = newState;
+    switch (currentState)
+    {
+    case AppState::Empty:
+        break;
+    case AppState::CreatingTask:
+        break;
+    }
+}
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    switch (currentState)
+    {
+    case AppState::Empty:
+        if (event->key() == Qt::Key_N)
+        {
+            setState(AppState::CreatingTask);
+        }
+        break;
+    case AppState::CreatingTask:
+        if(event->key() == Qt::Key_Escape)
+        {
+            setState(AppState::Empty);
+        }
+        break;
+    }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,15 +1,22 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
 #include <QMainWindow>
-
+#include <QKeyEvent>
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
+protected:
+    void keyPressEvent(QKeyEvent *event) override;
+private:
+    enum class AppState
+    {
+        Empty,
+        CreatingTask
+    };
+    AppState currentState;
+    void setState(AppState newState);
 };
 #endif // MAINWINDOW_H
