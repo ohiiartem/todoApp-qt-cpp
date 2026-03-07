e291b37e295ae417f91cbdc47c2f9017b314c616
Artem <ogijartem7@gmail.com>
2026-03-07 13:46:20 +0100

    feat: add hint labels and central widget layout

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,28 +1,75 @@
 #include "mainwindow.h"
+
 #include <QKeyEvent>
+#include <QLabel>
+#include <QLineEdit>
+#include <QVBoxLayout>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
     currentState(AppState::Empty)
 {
     setFixedSize(1400, 750);
+
+    hintLabel = new QLabel();
+    hintLabel->setAlignment(Qt::AlignCenter);
+
+    subHintLabel = new QLabel();
+    subHintLabel->setAlignment(Qt::AlignCenter);
+
+
+    QVBoxLayout *layout = new QVBoxLayout();
+    layout->addStretch();
+    layout->addWidget(hintLabel);
+    layout->addSpacing(10);
+    layout->addWidget(subHintLabel);
+    layout->addStretch();
+
+    layout->setAlignment(Qt::AlignCenter);
+
+    QWidget *centralWidget = new QWidget(this);
+    centralWidget->setLayout(layout);
+    setCentralWidget(centralWidget);
+
+
+
+
     setState(AppState::Empty);
+
+
 }
+
 MainWindow::~MainWindow() {}
+
+
 void MainWindow::setState(AppState newState)
 {
     currentState = newState;
+
     switch (currentState)
     {
     case AppState::Empty:
+        hintLabel->setText("Press N");
+        subHintLabel->setText("to create a task");
+
+        hintLabel->show();
+        subHintLabel->show();
+
         break;
     case AppState::CreatingTask:
+        hintLabel->setText("ESC");
+        subHintLabel->setText("");
         break;
     }
 }
+
+
 void MainWindow::keyPressEvent(QKeyEvent *event)
 {
+
     switch (currentState)
     {
+
     case AppState::Empty:
         if (event->key() == Qt::Key_N)
         {
@@ -35,5 +82,13 @@ void MainWindow::keyPressEvent(QKeyEvent *event)
             setState(AppState::Empty);
         }
         break;
+
     }
 }
+
+
+
+
+
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,21 +2,42 @@
 #define MAINWINDOW_H
 #include <QMainWindow>
 #include <QKeyEvent>
+
+#include <QLabel>
+#include <QLineEdit>
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
 protected:
     void keyPressEvent(QKeyEvent *event) override;
+
 private:
     enum class AppState
     {
         Empty,
         CreatingTask
     };
+
+
+    QLabel *hintLabel;
+    QLabel *subHintLabel;
+
+
+
     AppState currentState;
     void setState(AppState newState);
 };
 #endif // MAINWINDOW_H
+
+
+
+
+
+
+
