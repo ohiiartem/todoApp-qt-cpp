663669232723ced5a7aba85660e6f8eb2dc0bf8d
Artem <ogijartem7@gmail.com>
2026-08-12 18:12:05 +0200

    Add test for removeTask

--- tests/TaskManagerTest.cpp
+++ tests/TaskManagerTest.cpp
@@ -11,3 +11,13 @@ TEST(TaskManagerTest, AddTaskIncreasesCount)
     EXPECT_EQ("buy bread", manager.taskAt(0).text());
     EXPECT_FALSE(manager.taskAt(0).isCompleted());
 }
+
+TEST(TaskManagerTest, RemoveTaskDecreasesCount)
+{
+    TaskManager manager;
+
+    manager.addTask("buy bread");
+    manager.removeTask(0);
+
+    EXPECT_EQ(0, manager.taskCount());
+}
